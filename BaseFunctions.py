980d0ced7894b8f47c92a30b72ed1ebeab04e1fc
Siidero <bastien78@live.fr>
2019-08-05 10:53:12 +0000

    base functions

---
+++ BaseFunctions.py
@@ -0,0 +1,35 @@
+import time;
+import explorerhat;
+
+#functions
+def forward(speed, temp):
+    curr_time = time.time()
+    final_time = curr_time + temp
+    while curr_time <= final_time:
+        curr_time = time.time()
+        explorerhat.motor.two.forward(speed)
+        explorerhat.motor.one.backward(speed)
+        
+def backward(speed, temp):
+    curr_time = time.time()
+    final_time = curr_time + temp
+    while curr_time <= final_time:
+        curr_time = time.time()
+        explorerhat.motor.one.forward(speed)
+        explorerhat.motor.two.backward(speed)
+    
+def turn_right(speed, temp):
+    curr_time = time.time()
+    final_time = curr_time + temp
+    while curr_time <= final_time:
+        curr_time = time.time()
+        explorerhat.motor.one.forward(speed)
+        explorerhat.motor.two.forward(speed)
+
+def turn_left(speed, temp):
+    curr_time = time.time()
+    final_time = curr_time + temp
+    while curr_time <= final_time:
+        curr_time = time.time()
+        explorerhat.motor.one.backward(speed)
+        explorerhat.motor.two.backward(speed)
