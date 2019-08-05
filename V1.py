ae425097a6a78e1f139fc5750906601ba08d1210
Siidero <bastien78@live.fr>
2019-08-05 10:34:55 +0000

    turn left/right

--- V1.py
+++ V1.py
@@ -17,5 +17,21 @@ def backward(speed, temp):
         curr_time = time.time()
         explorerhat.motor.one.forward(speed)
         explorerhat.motor.two.backward(speed)
+    
+def turn_right(speed, temp):
+    curr_time = time.time()
+    final_time = curr_time + temp
+    while curr_time <= final_time:
+        curr_time = time.time()
+        explorerhat.motor.one.forward(speed)
+        explorerhat.motor.two.forward(speed)
+
+def turn_left(speed, temp):
+    curr_time = time.time()
+    final_time = curr_time + temp
+    while curr_time <= final_time:
+        curr_time = time.time()
+        explorerhat.motor.one.backward(speed)
+        explorerhat.motor.two.backward(speed)
 #main
-backward(50,1)
+turn_right(50,1)
