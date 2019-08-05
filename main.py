16b11cefc72c147056d73dfc90b329caf1c78343
root <root@localhost.localdomain>
2019-08-05 13:44:37 +0200

    Threads

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 #import all of files
+from threading import Thread
 import BaseFunctions
 
 #use functions of BaseFunctions
+class Streaming(Thread):		# Classe qui servira a lancer le stream en parallèle
+	def __init__(self):
+		Thread.__init__(self)
+	
+	def run(self):
+		import streaming
+
+
+thread_stream = Streaming()
+thread_stream.start()
 BaseFunctions.turn_left(50,1)
 BaseFunctions.turn_right(50,1)
 BaseFunctions.forward(50,1)
