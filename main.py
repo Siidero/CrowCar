0ecc3f7bedc07d05307e25bb6e46bac365945799
Siidero <bastien78@live.fr>
2019-08-05 11:12:26 +0000

    add main file and import

---
+++ main.py
@@ -0,0 +1,8 @@
+#import all of files
+import BaseFunctions
+
+#use functions of BaseFunctions
+BaseFunctions.turn_left(50,1)
+BaseFunctions.turn_right(50,1)
+BaseFunctions.forward(50,1)
+BaseFunctions.backward(50,1)
